98fd1d9f62c0d8c068ad3b1b985ffd6367ed695d
at2224 <at2224@njit.edu>
2024-10-30 19:17:08 -0400

    starting script

---
+++ script.js
@@ -0,0 +1,3 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c418f685cc0e2da3a17f9e154c901f38&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=c418f685cc0e2da3a17f9e154c901f38&query=';
