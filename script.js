456d761d856e6e3bb032abefbb9816dd8d73d3eb
at2224 <at2224@njit.edu>
2024-10-31 14:09:35 -0400

    adding js script to automatically add cards to every movie in search result

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c418f685cc0e2da3a17f9e154c901f38&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=c418f685cc0e2da3a17f9e154c901f38&query=';
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+returnMovies(APILINK)
+function returnMovies(url) {
+    fetch(url).then(res => res.json()).then(function(data) {
+        console.log(data.results);
+        data.results.forEach(element => {
+            const div_card = document.createElement('div')
+            const div_row = document.createElement('div')
+            const div_column = document.createElement('div')
+            const image = document.createElement('img');
+            const title = document.createElement('h3');
+            const center = document.createElement('center');
+            
+            title.innerHTML = `${element.title}`
+            image.src = IMG_PATH + element.poster_path
+
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+        });
+    });
+}
