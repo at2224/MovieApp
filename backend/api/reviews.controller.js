2f606c8de24975622a7f697f0bfa705ed7bff878
at2224 <at2224@njit.edu>
2024-12-22 12:51:36 -0500

    backend w/ express node js mongo db atlas. Connected to db

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -8,9 +8,9 @@ export default class ReviewsController {
             const user = req.body.user
             console.log('movieid', movieId)
             const reviewResponse = await ReviewsDAO.addReview(
-            movieId,
-            user,
-            review
+                movieId,
+                user,
+                review
             )
             res.json({ status: "success" })
         } catch (e) {
@@ -23,46 +23,46 @@ export default class ReviewsController {
             let id = req.params.id || {}
             let review = await ReviewsDAO.getReview(id)
             if (!review) {
-            res.status(404).json({ error: "Not found" })
-            return
+                res.status(404).json({ error: "Not found" })
+                return
             }
             res.json(review)
         } catch (e) {
             console.log(`api, ${e}`)
             res.status(500).json({ error: e })
         }
-        }
+    }
 
-        static async apiUpdateReview(req, res, next) {
+    static async apiUpdateReview(req, res, next) {
         try {
             const reviewId = req.params.id
             const review = req.body.review
             const user = req.body.user
 
             const reviewResponse = await ReviewsDAO.updateReview(
-            reviewId,
-            user,
-            review
+                reviewId,
+                user,
+                review
             )
 
             var { error } = reviewResponse
             if (error) {
-            res.status(400).json({ error })
+                res.status(400).json({ error })
             }
 
             if (reviewResponse.modifiedCount === 0) {
-            throw new Error(
-                "unable to update review",
-            )
+                throw new Error(
+                    "unable to update review",
+                )
             }
 
             res.json({ status: "success" })
         } catch (e) {
             res.status(500).json({ error: e.message })
         }
-        }
+    }
 
-        static async apiDeleteReview(req, res, next) {
+    static async apiDeleteReview(req, res, next) {
         try {
             const reviewId = req.params.id
             const reviewResponse = await ReviewsDAO.deleteReview(reviewId)
@@ -70,20 +70,20 @@ export default class ReviewsController {
         } catch (e) {
             res.status(500).json({ error: e.message })
         }
-        }
+    }
 
-        static async apiGetReviews(req, res, next) {
+    static async apiGetReviews(req, res, next) {
         try {
             let id = req.params.id || {}
             let reviews = await ReviewsDAO.getReviewsByMovieId(id)
             if (!reviews) {
-            res.status(404).json({ error: "Not found" })
-            return
+                res.status(404).json({ error: "Not found" })
+                return
             }
             res.json(reviews)
         } catch (e) {
             console.log(`api, ${e}`)
             res.status(500).json({ error: e })
         }
-        }
     }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-//import ReviewsDAO from "./dao/reviewsDAO.js"
-import dotenv from 'dotenv';
-dotenv.config();
+import ReviewsDAO from "./dao/reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
-const mongo_username = process.env['MONGO_USERNAME'];
-const mongo_password = process.env['MONGO_PASSWORD'];
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.jbyvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const mongo_username = process.env['MONGO_USERNAME']
+const mongo_password = process.env['MONGO_PASSWORD']
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.ayr9czp.mongodb.net/?retryWrites=true&w=majority`
 
-const port = 8000;
+const port = 8000
 
 MongoClient.connect(
     uri,
@@ -17,12 +15,14 @@ MongoClient.connect(
         maxPoolSize: 50,
         wtimeoutMS: 2500,
         useNewUrlParser: true
-    }
-).catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-}).then(async client => {
-    app.listen(port, () => {
-        console.log(`listening on port ${port}`)
     })
-})
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client => {
+        await ReviewsDAO.injectDB(client)
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
